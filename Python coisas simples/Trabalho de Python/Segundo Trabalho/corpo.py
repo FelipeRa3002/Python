d9fc0c46204d4338a8aeeff4d6d46e5ed2cb86ea
Felipe Rangel <136349846+FelipeRa3002@users.noreply.github.com>
2026-04-12 18:06:29 -0300

    Update corpo.py

--- Python coisas simples/Trabalho de Python/Segundo Trabalho/corpo.py
+++ Python coisas simples/Trabalho de Python/Segundo Trabalho/corpo.py
@@ -2,9 +2,5 @@ import funções
 #Música que eu que amei ouvir https://youtu.be/iMbPZLtgQR4?si=YDB3aFFtPYtvIVpk , música boa demais amei
 
 if __name__ == "__main__":
-    
- if funções.Menu() !=True:
-   funções.Menu()
- else:
    funções.Menu()
  

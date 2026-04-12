79e1ecb82fa4723a3c8ff14096380ded440091f0
Felipe Rangel <136349846+FelipeRa3002@users.noreply.github.com>
2026-04-12 10:06:10 -0300

    Add files via upload

---
+++ Python coisas simples/Trabalho de Python/Segundo Trabalho/corpo.py
@@ -0,0 +1,6 @@
+import funções
+#Música que eu que amei ouvir https://youtu.be/iMbPZLtgQR4?si=YDB3aFFtPYtvIVpk , música boa demais amei
+
+if __name__ == "__main__":
+    
+ funções.Menu()
---
+++ Python coisas simples/Trabalho de Python/Segundo Trabalho/dados.py
@@ -0,0 +1,3 @@
+# Foi modificado no dia 12/04/2026 às 10:05:30 pelo Felipe
+listadaTurma = []
+mediadaTurmaDefinitiva= 0.0

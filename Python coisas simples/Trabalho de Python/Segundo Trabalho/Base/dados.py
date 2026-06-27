0551c58bf340de905d224befc91e62f2c36f0edb
Felipe Rangel <136349846+FelipeRa3002@users.noreply.github.com>
2026-06-27 14:06:02 -0300

    Adiciona integração da biblioteca rich
    Adiciona integração da biblioteca rich para melhorar a interface do terminal, a exibição de tabelas e a apresentação visual das informações do sistema em Python
    
    Esse commit inclui a configuração inicial da biblioteca rich no projeto, melhora a formatação das saídas no terminal e prepara a aplicação para mostrar dados de forma mais organizada, legível e agradável ao usuário.

---
+++ Python coisas simples/Trabalho de Python/Segundo Trabalho/Base/dados.py
@@ -0,0 +1,30 @@
+# Foi modificado no dia 27/06/2026 às 13:36:26 pelo Felipe
+listadaTurma = [
+ {'nome': 'Makima', 'nota1': 10.0, 'nota2': 10.0, 'media': 10.0, 'situação': 'Aprovado'} #Está na possição 0
+, {'nome': 'Zero two', 'nota1': 5.0, 'nota2': 5.0, 'media': 5.0, 'situação': 'Reprovado'} #Está na possição 1
+, {'nome': 'Ami mizuno (sailor mercury)', 'nota1': 10.0, 'nota2': 10.0, 'media': 10.0, 'situação': 'Aprovado'} #Está na possição 2
+, {'nome': 'Usagi tsukino (sailor moon)', 'nota1': 4.0, 'nota2': 5.0, 'media': 4.5, 'situação': 'Reprovado'} #Está na possição 3
+, {'nome': 'Kaguya shinomiya', 'nota1': 9.8, 'nota2': 10.0, 'media': 9.9, 'situação': 'Aprovado'} #Está na possição 4
+, {'nome': 'Chika fujiwara', 'nota1': 6.5, 'nota2': 7.5, 'media': 7.0, 'situação': 'Aprovado'} #Está na possição 5
+, {'nome': 'Aqua (konosuba)', 'nota1': 2.0, 'nota2': 1.0, 'media': 1.5, 'situação': 'Reprovado'} #Está na possição 6
+, {'nome': 'Megumin', 'nota1': 8.5, 'nota2': 6.0, 'media': 7.25, 'situação': 'Aprovado'} #Está na possição 7
+, {'nome': 'Rin tohsaka', 'nota1': 9.5, 'nota2': 9.0, 'media': 9.25, 'situação': 'Aprovado'} #Está na possição 8
+, {'nome': 'Saber', 'nota1': 8.0, 'nota2': 8.5, 'media': 8.25, 'situação': 'Aprovado'} #Está na possição 9
+, {'nome': 'Nezuko kamado', 'nota1': 7.0, 'nota2': 7.0, 'media': 7.0, 'situação': 'Aprovado'} #Está na possição 10
+, {'nome': 'Mitsuri kanroji', 'nota1': 6.0, 'nota2': 6.5, 'media': 6.25, 'situação': 'Reprovado'} #Está na possição 11
+, {'nome': 'Yoruichi shihoin', 'nota1': 8.5, 'nota2': 8.5, 'media': 8.5, 'situação': 'Aprovado'} #Está na possição 12
+, {'nome': 'Rukia kuchiki', 'nota1': 8.0, 'nota2': 7.5, 'media': 7.75, 'situação': 'Aprovado'} #Está na possição 13
+, {'nome': 'Tsunade', 'nota1': 7.5, 'nota2': 8.0, 'media': 7.75, 'situação': 'Aprovado'} #Está na possição 14
+, {'nome': 'Sakura haruno', 'nota1': 9.5, 'nota2': 9.5, 'media': 9.5, 'situação': 'Aprovado'} #Está na possição 15
+, {'nome': 'Hinata hyuga', 'nota1': 8.0, 'nota2': 8.0, 'media': 8.0, 'situação': 'Aprovado'} #Está na possição 16
+, {'nome': 'Mikasa ackerman', 'nota1': 9.0, 'nota2': 8.5, 'media': 8.75, 'situação': 'Aprovado'} #Está na possição 17
+, {'nome': 'Sasha blouse', 'nota1': 5.0, 'nota2': 5.5, 'media': 5.25, 'situação': 'Reprovado'} #Está na possição 18
+, {'nome': 'Hange zoë', 'nota1': 9.8, 'nota2': 9.5, 'media': 9.65, 'situação': 'Aprovado'} #Está na possição 19
+, {'nome': 'Nami', 'nota1': 9.0, 'nota2': 9.5, 'media': 9.25, 'situação': 'Aprovado'} #Está na possição 20
+, {'nome': 'Nico robin', 'nota1': 10.0, 'nota2': 10.0, 'media': 10.0, 'situação': 'Aprovado'} #Está na possição 21
+, {'nome': 'Boa hancock', 'nota1': 8.0, 'nota2': 7.0, 'media': 7.5, 'situação': 'Aprovado'} #Está na possição 22
+, {'nome': 'Power', 'nota1': 3.0, 'nota2': 2.0, 'media': 2.5, 'situação': 'Reprovado'} #Está na possição 23
+, {'nome': 'Yor forger', 'nota1': 7.0, 'nota2': 7.5, 'media': 7.25, 'situação': 'Aprovado'} #Está na possição 24
+
+]
+mediadaTurmaDefinitiva= 187.6
